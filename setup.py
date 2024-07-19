7403f0bd3555630c313e11538c9acd5339dd289b
monshinawatra <monshinawatra@gmail.com>
2024-07-20 02:41:00 +0700

    Test

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='flashcard_backend',
     version='0.8',
-    packages=find_packages(),
+    packages=find_packages(where='flashcard_backend'),
+    package_dir={"": 'flashcard_backend'},
+    url="https://github.com/Alpaca-8b-Llama3Hackathon/llama3-backend"
+    
+    # package_dir={'flashcard_backend': 'flashcard_backend'},
+    # package_data={'flashcard_backend': ['*.py']},
     # install_requires=[
     #     "llama-index",
     #     "llama-index-embeddings-together",
