a921ce143a162fe24b749a3c129f701dd23cc17d
monshinawatra <monshinawatra@gmail.com>
2024-07-20 21:02:19 +0700

    fix document mixing

--- flashcard_backend/question/main.py
+++ flashcard_backend/question/main.py
@@ -2,6 +2,7 @@ from llama_index.llms.openai_like import OpenAILike
 from llama_index.core.node_parser import SentenceSplitter
 from PyPDF2 import PdfReader
 import os
+from llama_index.core import Document
 from llama_index.core.extractors import (
     QuestionsAnsweredExtractor,
     TitleExtractor,
